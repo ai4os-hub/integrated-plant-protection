234e1eb2555a1451494c82cbcac58bf66efb04ce
Jedrzej Smok <jedrzejsmok55@gmail.com>
2025-07-19 15:18:10 +0200

    fix

--- tests/test_api.py
+++ tests/test_api.py
@@ -38,7 +38,7 @@ class TestModelMethods(unittest.TestCase):
         )
         self.assertEqual(
             self.meta["license"].lower(),
-            "MIT License".lower(),
+            "MIT".lower(),
         )
 
 
